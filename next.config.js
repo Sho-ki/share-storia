9c42675d9090610d29b2148cc641cca73c2308e5
Sho-ki <shoki0116.highjump@gmail.com>
2023-04-13 00:05:37 -0700

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['randomuser.me'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
